79ee2d90c1732043b8c600a8babde980da532b63
altea <altea.baumann@outlook.com>
2023-01-26 20:07:07 +0100

    Neuer Versuch

--- login.js
+++ login.js
@@ -1,19 +1,24 @@
-/**Validierung der Email.
- * Wenn es das RegEx (Mengen von Zeichenketten mit Hilfe bestimmter syntaktischer Regeln) nicht erfüllt wird rot**/
+/**HTML DOM**/
 let emailElement=document.querySelector("[type=email]")
 let passwordElement=document.querySelector("[type=password]")
 const submitElement=document.querySelector("[type=submit]")
+
+/**Validation Email und Passwort**/
 function validateForm(){
    const emailValid= validateEmail()
    const passwordValid= validatePassword()
    if (emailValid & passwordValid){
-      submitElement.removeAttribute("disabled")
+      submitElement.removeAttribute("disabled");
+
    }
   else{
       submitElement.setAttribute("disabled","disabled")
 
    }
 }
+
+
+/**Validation Email mit der RegEx Funktion**/
 function validateEmail() {
 
 const email=emailElement.value
@@ -28,11 +33,16 @@ let result =testEmailRegEx(email)
    }
 return result
 }
+
+/**Test Email Funktion RegEx**/
 function testEmailRegEx(email) {
    let emailpattern=/^[a-z._-]{3,}@[a-z_-]{3,}\.\w{2,3}$/gi;
    return emailpattern.test(email);
 }
-
+/**Funktion init
+ *addEventListener ist eine zentrale Funktion, die Events und die dazugehörigen Aktionen registriert.
+ *addEventListener behandelt Events für window, document oder individuelle Elemente der Webseite.
+ *removeEventListener löst den Event Handler wieder vom Ereignis.**/
 
 function init(){
    console.log("init")
@@ -40,6 +50,9 @@ function init(){
     emailElement.addEventListener("input",validateForm)
    passwordElement.addEventListener("input",validateForm)
 }
+
+
+/**Validation Passwort**/
 function validatePassword() {
    let password =passwordElement.value;
    let result =testpasswordlength(password,12) ;
@@ -59,6 +72,10 @@ function validatePassword() {
 
 return finalResult
 }
+
+
+/**Validation Passwort (Länge) RegEx Funktion
+ * Regular Expression Validation**/
 function testpasswordlength (password,length) {
    return password.length >= length;
 
@@ -70,7 +87,7 @@ function testNumberRegEx(password) {
 
 function testSpecialCharacters(password) {
    return /[\u0021--\u0029]+/g.test(password);
-unicode-table.com
+
 
 }
 
